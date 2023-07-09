7d38964f47d901423d817353eb91bcbfbb545be7
Bryan Miller <bmiller1008@gmail.com>
2023-07-08 23:14:28 -0400

    Rewrote the Enemy physics process to use 'overrideable methods' that children to use to alter/add to the core enemy system. Updated Xeno to use the new Enemy 'override methods'. Started implementing a basic state system (Patrolling works. Searching technically works, but spazzes out. Other states not yet implemented).

--- objects/enemy/enemy.gd
+++ objects/enemy/enemy.gd
@@ -27,6 +27,11 @@ var _action : StringName = &"idle"
 var _facing : Vector2 = Vector2.DOWN
 var _direction : Vector2 = Vector2.ZERO
 
+var _face_direction : bool = true
+var _facing_offset : float = 0.0
+
+var _speed_multiplier : float = 1.0
+
 var _body : AnimatedSprite2D = null
 var _sprite_frames : SpriteFrames = null
 
@@ -38,21 +43,39 @@ func _ready() -> void:
 	_UpdateViz()
 
 
-#func _physics_process(_delta : float) -> void:
-#	if _direction.length_squared() > DIRECTIONAL_THRESHOLD:
-#		velocity = _direction * max_speed
-#	else:
-#		velocity = lerp(velocity, Vector2.ZERO, 0.7)
-#
-#	if move_and_slide():
-#		collided.emit(get_last_slide_collision(), get_slide_collision_count())
-#
-#	_action = &"move" if velocity.length() > 0.1 else &"idle"
-#	_UpdateViz()
+func _physics_process(delta : float) -> void:
+	if Engine.is_editor_hint(): return
+	_Process_Start(delta)
+	_Process_Velocity(delta)
+	_Process_Move(delta)
+	_Process_End(delta)
+
+# ------------------------------------------------------------------------------
+# Private Overrideable Methods
+# ------------------------------------------------------------------------------
+func _Process_Start(_delta : float) -> void:
+	pass
+
+func _Process_Velocity(delta : float) -> void:
+	if _direction.length_squared() > DIRECTIONAL_THRESHOLD:
+		var weight : float = 1.0 if not _face_direction else _CalculateFacing(delta)
+		#print("Weight: ", weight, " | Multiplier: ", _speed_multiplier)
+		velocity = _direction * max_speed * weight * _speed_multiplier
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, 0.7)
+
+func _Process_Move(_delta : float) -> void:
+	if move_and_slide():
+		collided.emit(get_last_slide_collision(), get_slide_collision_count())
+
+func _Process_End(_delta : float) -> void:
+	_action = &"move" if velocity.length() > 0.1 else &"idle"
+	_UpdateViz()
 
 # ------------------------------------------------------------------------------
 # Private Methods
 # ------------------------------------------------------------------------------
+
 func _SetBodySprite(body_sprite : AnimatedSprite2D) -> void:
 	_body = body_sprite
 
@@ -63,6 +86,24 @@ func _GetSpriteFrames() -> SpriteFrames:
 	# TODO: Code section for swapping atlas texture!!
 	
 	return _sprite_frames
+
+func _ShortAngleDist(from : float, to: float) -> float:
+	var diff : float = fmod(to - from, PI * 2)
+	return fmod(2 * diff, PI * 2) - diff
+
+func _CalculateFacing(delta : float) -> float:
+	var rps : float = deg_to_rad(360.0) * delta
+	var d : float = _ShortAngleDist(_facing.angle(), _direction.angle())
+	
+	var weight : float = 1.0
+	if d >= deg_to_rad(0.5):
+		weight = min(1.0, rps / d)
+	
+	var nfacing : Vector2 = _direction
+	if weight < 0.98:
+		nfacing = Vector2.RIGHT.rotated(lerp(_facing.angle(), _direction.angle(), weight))
+	_facing = nfacing.rotated(_facing_offset)
+	return weight
 
 func _CastTo(pos : Vector2, coll_mask : int = 4294967295) -> Dictionary:
 	return  get_world_2d().direct_space_state.intersect_ray(
@@ -100,18 +141,38 @@ func _UpdateViz() -> void:
 # ------------------------------------------------------------------------------
 # Public Methods
 # ------------------------------------------------------------------------------
-func face_position(pos : Vector2) -> void:
-	if pos.is_equal_approx(position): return
-	_facing = position.direction_to(pos)
-	_UpdateViz()
+func set_face_direction(e : bool) -> void:
+	_face_direction = e
 
-func move(dir : Vector2) -> void:
-	_direction = dir
+func is_facing_direction() -> bool:
+	return _face_direction
+
+func set_facing_offset(offset : float) -> void:
+	offset = fmod(offset, PI * 2)
+	_facing_offset = offset
+
+func get_facing_offset() -> float:
+	return _facing_offset
+
+func set_facing(f : Vector2) -> void:
+	_facing = f.normalized()
+
+func get_facing() -> Vector2:
+	return _facing
+
+func set_speed_multiplier(sm : float) -> void:
+	_speed_multiplier = max(0.0, min(1.0, sm))
+
+func get_speed_multiplier() -> float:
+	return _speed_multiplier
+
+func set_direction(d : Vector2) -> void:
+	_direction = d
 
 func get_direction() -> Vector2:
 	return _direction
 
-func damage(amount : float) -> void:
+func damage(_amount : float) -> void:
 	print("Ouch!")
 
 
--- objects/enemy/xeno/xeno.gd
+++ objects/enemy/xeno/xeno.gd
@@ -11,12 +11,20 @@ extends "res://objects/enemy/enemy.gd"
 # ------------------------------------------------------------------------------
 const FRAMES_XENO : SpriteFrames = preload("res://objects/enemy/xeno/xeno_frames.tres")
 
+const MEM_NEST : String = "nest"
+const MEM_SEARCHING : String = "searching"
+const MEM_PATROLLING : String = "patrolling"
+const MEM_HUNTING : String = "hunting"
+
+enum STATE {Nest = 0, Searching = 1, Patrolling = 2, Hunting = 3}
 
 # ------------------------------------------------------------------------------
 # Export Variables
 # ------------------------------------------------------------------------------
 @export_category("Xeno")
 @export var nav_point_group : String = ""
+@export_range(1, 360) var search_degrees_per_second : float = 10.0
+@export var debug : bool = false
 
 # ------------------------------------------------------------------------------
 # Variables
@@ -24,6 +32,9 @@ const FRAMES_XENO : SpriteFrames = preload("res://objects/enemy/xeno/xeno_frames
 var _nav_group : StringName = &""
 var _target_nav_point : NavPoint = null
 var _last_target_pos : Vector2 = Vector2.ZERO
+
+var _state : STATE = STATE.Patrolling
+var _state_mem : Dictionary = {}
 
 # ------------------------------------------------------------------------------
 # Onready Variables
@@ -41,53 +52,156 @@ func set_nav_point_group(npg : String) -> void:
 		nav_point_group = npg
 		_target_nav_point = Game.get_random_nav_point_in_group(nav_point_group)
 
+func set_debug(d : bool) -> void:
+	if d != debug:
+		debug = d
+		if _nav_agent != null:
+			_nav_agent.debug_enabled = debug
+
 # ------------------------------------------------------------------------------
 # Override Methods
 # ------------------------------------------------------------------------------
 func _ready() -> void:
+	_nav_agent.debug_enabled = debug
+	set_face_direction(true)
 	_SetSpriteFrames(FRAMES_XENO)
 	_SetBodySprite($ASprite2D)
 	super._ready()
 
 func _draw() -> void:
+	if not debug: return
 	if _target_nav_point == null: return
 	var target_pos : Vector2 = _target_nav_point.global_position - global_position
 	draw_line(Vector2.ZERO, target_pos, Color.VIOLET, 1, true)
 
-func _physics_process(delta : float) -> void:
-	if Engine.is_editor_hint(): return
-	_TrackNavPoints(delta)
-	
-	if _direction.length() > DIRECTIONAL_THRESHOLD:
-		var weight : float = _CalculateFacingWeight(delta)
-		_facing = Vector2.RIGHT.rotated(lerp(_facing.angle(), _direction.angle(), weight))
-		velocity = _direction * max_speed * weight
-	else:
-		velocity = lerp(velocity, Vector2.ZERO, 0.7)
-	
-	if move_and_slide():
-		collided.emit(get_last_slide_collision(), get_slide_collision_count())
+# ------------------------------------------------------------------------------
+# Private Overrideable Methods
+# ------------------------------------------------------------------------------
+func _Process_Start(delta : float) -> void:
+	match _state:
+		STATE.Nest:
+			_State_Nest(delta)
+		STATE.Searching:
+			_State_Searching(delta)
+		STATE.Patrolling:
+			_State_Patrolling(delta)
+		STATE.Hunting:
+			_State_Hunting(delta)
+	#_TrackNavPoints(delta)
 
+func _Process_End(delta : float) -> void:
 	_sight_area.rotation = _facing.angle()
-	_action = &"move" if velocity.length() > 0.1 else &"idle"
-	_UpdateViz()
 	queue_redraw()
+	super._Process_End(delta)
+
+
+# ------------------------------------------------------------------------------
+# Private State Handling Methods
+# ------------------------------------------------------------------------------
+func _State_Nest(_delta : float) -> void:
+	# TODO: Actually handle this state. For now, just bounce to Patrolling
+	_state = STATE.Patrolling
+
+func _State_Searching(delta : float) -> void:
+	var dlength : float = _direction.length()
+	print("Searching")
+	if not MEM_SEARCHING in _state_mem:
+		var angle : float = deg_to_rad(45.0)
+		_state_mem[MEM_SEARCHING] = {
+			"offset": angle if randf() < 0.5 else -angle,
+			"scan_count": randi_range(1, 4),
+			"facing": _facing if dlength < 0.01 else _direction.normalized()
+		}
+		set_speed_multiplier(0.1)
+	
+	var mem : Dictionary = _state_mem[MEM_SEARCHING]
+	
+	var rps : float = deg_to_rad(search_degrees_per_second)
+	var off : float = get_facing_offset()
+	var d : float = _ShortAngleDist(off, mem.offset)
+	var weight : float = 1.0
+	if d >= deg_to_rad(0.5):
+		weight = min(1.0, rps / d)
+	var angle : float = lerpf(off, mem.offset, weight)
+	set_facing_offset(angle)
+	if dlength <= 0.01:
+		set_facing(mem.facing.rotated(angle))
+	
+	if weight > 0.998:
+		if mem.offset != 0.0:
+			mem.scan_count -= 1
+			if mem.scan_count > 0:
+				mem.offset = -mem.offset
+			else:
+				mem.offset = 0.0
+		else:
+			set_facing_offset(0.0)
+			_state_mem.erase(MEM_SEARCHING)
+			_state = STATE.Patrolling if randf() < 0.95 else STATE.Nest
+	elif MEM_PATROLLING in _state_mem:
+		_State_Patrolling(delta)
+
+func _State_Patrolling(_delta : float) -> void:
+	print("Patrolling")
+	if not MEM_PATROLLING in _state_mem:
+		_state_mem[MEM_PATROLLING] = {
+			"last_target_pos": global_position,
+			"target_nav_point": weakref(null)
+		}
+	if not MEM_SEARCHING in _state_mem:
+		set_speed_multiplier(0.25)
+	
+	var mem : Dictionary = _state_mem[MEM_PATROLLING]
+	
+	var nav_point : NavPoint = mem.target_nav_point.get_ref()
+	if nav_point == null:
+		mem.last_target_pos = global_position
+		nav_point = Game.get_random_nav_point_in_group(nav_point_group)
+		mem.target_nav_point = weakref(nav_point)
+	
+	if not _nav_agent.target_position.is_equal_approx(nav_point.global_position):
+		_nav_agent.target_position = nav_point.global_position
+		set_direction(Vector2.ZERO)
+	elif not _nav_agent.is_target_reachable():
+		mem.target_nav_point = weakref(null)
+	elif _nav_agent.is_target_reached():
+		_HandleNavpointReached()
+	else:
+		var target_pos : Vector2 = _nav_agent.get_next_path_position()
+		if not target_pos.is_equal_approx(mem.last_target_pos):
+			_direction = global_position.direction_to(target_pos)
+			mem.last_target_pos = target_pos
+	
+	if not MEM_SEARCHING in _state_mem and randf() < 0.05:
+		_state = STATE.Searching
+
+
+func _State_Hunting(_delta : float) -> void:
+	# TODO: Actually handle this state. For now, just bounce to Patrolling
+	_state = STATE.Patrolling
 
 # ------------------------------------------------------------------------------
 # Private Methods
-# --------------------------------------------------------------a---------------
-
-func _CalculateFacingWeight(delta : float) -> float:
-	var rps : float = deg_to_rad(180.0) * delta
-	var fangle : float = _facing.angle()
-	var dangle : float = _direction.angle()
-	var d : float = abs(fangle - dangle)
-	if abs(dangle - fangle) < d:
-		d = abs(dangle - fangle)
-	
-	if d < deg_to_rad(0.5):
-		return 1.0
-	return min(1.0, rps / d)
+# ------------------------------------------------------------------------------
+func _HandleNavpointReached() -> void:
+	match _state:
+		STATE.Patrolling:
+			var rnd : float = randf()
+			if rnd < -0.08: # Remove the negative to reactivate
+				_state = STATE.Nest
+			elif rnd < 0.3:
+				_state_mem.erase(MEM_PATROLLING)
+				_state = STATE.Searching
+			else:
+				_state_mem[MEM_PATROLLING].target_nav_point = weakref(null)
+				# Otherwise... continue patrolling
+		STATE.Searching:
+			# Patrolling done, let's let searching finish.
+			_state_mem.erase(MEM_PATROLLING)
+		STATE.Hunting:
+			# If we reached player... keep hunting.
+			# Else if we reached last known position... search
+			pass
 
 func _TrackNavPoints(delta : float) -> void:
 	if _target_nav_point == null:
@@ -103,14 +217,12 @@ func _TrackNavPoints(delta : float) -> void:
 		if _nav_agent.is_target_reached():
 			_target_nav_point = null
 		
-		var dist : float = _last_target_pos.distance_to(global_position)
-		if dist <= 8.0:
-			var target_pos : Vector2 = _nav_agent.get_next_path_position()
-			if not target_pos.is_equal_approx(_last_target_pos):
-				_direction = global_position.direction_to(target_pos)
-				_last_target_pos = target_pos
-			elif not _target_nav_point == null:
-				_nav_agent.target_position = _target_nav_point.global_position
+		var target_pos : Vector2 = _nav_agent.get_next_path_position()
+		if not target_pos.is_equal_approx(_last_target_pos):
+			_direction = global_position.direction_to(target_pos)
+			_last_target_pos = target_pos
+		elif not _target_nav_point == null:
+			_nav_agent.target_position = _target_nav_point.global_position
 
 # ------------------------------------------------------------------------------
 # Public Methods
