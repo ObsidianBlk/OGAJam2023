d43b61aa24b0fbe4fefab8b285aef8c647a37e1c
Bryan Miller <bmiller1008@gmail.com>
2023-07-12 08:03:14 -0400

    Added a Starfield shader for use in the menu background. Adjusted the HellPlanet script to support control resizing as well as expose access to the shader(s) rotation parameter to the inspector.

--- scenes/menu_background/HellPlanet.gd
+++ scenes/menu_background/HellPlanet.gd
@@ -8,6 +8,7 @@ extends Control
 @export_category("Hell Planet")
 @export_range(0, 10) var seed = 1.0:						set = set_seed
 @export_range(-1.0, 1.0) var time_speed = 0.2:				set = set_time_speed
+@export_range(0.0, 6.28) var planet_rotation = 1.0:			set = set_planet_rotation
 
 
 # ------------------------------------------------------------------------------
@@ -34,16 +35,28 @@ func set_time_speed(s : float) -> void:
 		time_speed = s
 		_UpdateShaderParam("time_speed", time_speed)
 
+func set_planet_rotation(r : float) -> void:
+	if r >= 0.0 and r <= 6.28:
+		planet_rotation = r
+		_UpdateShaderParam("rotation", planet_rotation)
+
 # ------------------------------------------------------------------------------
 # Override Methods
 # ------------------------------------------------------------------------------
 func _ready() -> void:
 	set_seed(seed)
 	set_time_speed(time_speed)
+	set_planet_rotation(planet_rotation)
+	_UpdateSize()
 
 func _process(delta : float):
 	_time += delta
 	_UpdateShaderParam("time", _time)
+
+func _notification(what : int) -> void:
+	match what:
+		NOTIFICATION_RESIZED:
+			_UpdateSize()
 
 # ------------------------------------------------------------------------------
 # Private Methods
@@ -56,3 +69,8 @@ func _UpdateShaderParam(param : StringName, value : Variant) -> void:
 	if _rivers != null:
 		_rivers.material.set_shader_parameter(param, value)
 
+func _UpdateSize() -> void:
+	var size : Vector2 = get_size()
+	var pixels = min(size.x, size.y)
+	_UpdateShaderParam("pixels", pixels)
+	#_UpdateShaderParam("size", )
